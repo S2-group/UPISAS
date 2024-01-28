971581b77e0df1f7222b52f9778effff3e64aebb
Joel Milligan <joeldevelops@gmail.com>
2024-01-28 18:06:11 +0100

    Add email data and training method

--- mlops/training/train.py
+++ mlops/training/train.py
@@ -1,54 +1,104 @@
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
-from sklearn.metrics import accuracy_score, classification_report
+from sklearn.metrics import accuracy_score, classification_report, roc_curve, auc
+from sklearn.preprocessing import LabelEncoder
 import nltk
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 import string
+import re
 from joblib import dump
 
-# Example dataset
-emails = ["your free lottery ticket", "meeting request", ...]  # Add your email texts
-labels = [1, 0, ...]  # 1 for spam, 0 for not spam
+from versioning import get_next_version
 
 # Text preprocessing
-def preprocess(text):
-    text = text.lower()
-    text = ''.join([char for char in text if char not in string.punctuation])
-    words = text.split()
+def preprocess(body):
+    # Remove newlines
+    body = body.replace("\n", " ")
+    body = body.replace("\r", " ")
+
+    # Remove special characters and digits
+    body = re.sub(r'[^a-zA-Z\s]', '', body, re.I|re.A)
+
+    # Remove multiple spaces
+    body = re.sub(r'\s+', ' ', body)
+
+    body = body.lower()
+    body = ''.join([char for char in body if char not in string.punctuation])
+    words = body.split()
     words = [WordNetLemmatizer().lemmatize(word) for word in words if word not in stopwords.words('english')]
     return ' '.join(words)
 
-emails = [preprocess(email) for email in emails]
+def train(emails_df):
+    emails_df["processed_body"] = emails_df["body"].apply(preprocess)
 
-# Vectorization with bi-grams
-vectorizer = TfidfVectorizer(ngram_range=(1, 2))
-X = vectorizer.fit_transform(emails)
-y = np.array(labels)
+    # Vectorization with bi-grams
+    vectorizer = TfidfVectorizer(ngram_range=(1, 3))
 
-# Split dataset
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    # Encode labels
+    le = LabelEncoder()
+    emails_df["encoded_labels"] = le.fit_transform(emails_df["label"])
 
-# Train Naive Bayes model
-model = MultinomialNB()
-model.fit(X_train, y_train)
+    # Split dataset based on data_type
+    train_df = emails_df[emails_df["data_type"] == "train"]
+    test_df = emails_df[emails_df["data_type"] == "test"]
 
-# Evaluate model
-y_pred = model.predict(X_test)
-print("Accuracy:", accuracy_score(y_test, y_pred))
-print(classification_report(y_test, y_pred))
+    # Split into X and y
+    X_train = vectorizer.fit_transform(train_df["processed_body"])
+    y_train = train_df["encoded_labels"]
+    X_test = vectorizer.transform(test_df["processed_body"])
+    y_test = test_df["encoded_labels"]
+    # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Save model
-dump(model, "model@latest.joblib")
-dump(vectorizer, "vectorizer@latest.joblib")
+    # Train Naive Bayes model
+    model = MultinomialNB()
+    model.fit(X_train, y_train)
 
-# Predict new emails
-# def predict_email(new_email):
-#     processed_email = preprocess(new_email)
-#     vectorized_email = vectorizer.transform([processed_email])
-#     return model.predict(vectorized_email)[0]
+    # Evaluate model
+    y_pred = model.predict(X_test)
+    accuracy = accuracy_score(y_test, y_pred)
+    classification = classification_report(y_test, y_pred)
+    print("Accuracy:", accuracy)
+    print("Classification Report:", classification)
 
-# Example usage
-# print(predict_email("Congratulations, you've won a free cruise!"))
+    # Save model
+    dump(model, "./mlops/models/model@latest.joblib")
+    dump(vectorizer, "./mlops/models/vectorizer@latest.joblib")
+
+    # Save version of model as well
+    version = get_next_version()
+    dump(model, f"./mlops/models/model@{version}.joblib")
+    dump(vectorizer, f"./mlops/models/vectorizer@{version}.joblib")
+
+    # Get predicted probabilities
+    y_probs = model.predict_proba(X_test)[:, 1]
+
+    # Calculate ROC curve
+    fpr, tpr, thresholds = roc_curve(y_test, y_probs)
+
+    # Calculate AUC (Area under the ROC Curve)
+    roc_auc = auc(fpr, tpr)
+
+    # Plotting the ROC Curve
+    plt.figure()
+    plt.plot(fpr, tpr, color='darkorange', lw=2, label='ROC curve (area = %0.2f)' % roc_auc)
+    plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Receiver Operating Characteristic (ROC) Curve')
+    plt.legend(loc="lower right")
+    plt.savefig(f'./mlops/models/{version}_sensitivity_vs_specificity.png')
+
+    return accuracy, classification
+
+
+if __name__ == "__main__":
+    # train initial version
+    emails_df = pd.read_csv("./mlops/data/enron1.csv")
+    train(emails_df)
