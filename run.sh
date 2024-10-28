4ac123c4514fb2437f56a7bd87aa76aec8ef2b8c
Ilias Gerostathopoulos <iliasger@gmail.com>
2024-10-28 21:34:35 +0100

    fixing path to experiment runner

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export PYTHONPATH=$PYTHONPATH:/UPISAS
-python3 ../experiment-runner/experiment-runner/ UPISAS/experiment_runner_configs/SWIM_example.py
+python3 experiment-runner/experiment-runner/ UPISAS/experiment_runner_configs/SWIM_example.py
